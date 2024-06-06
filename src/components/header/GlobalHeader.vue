2db25f7e6cfb110d81395f946c37d213db9e7b9e
aaaaa-pi <770358634@qq.com>
2024-06-06 23:21:23 +0800

    feat：添加基本布局

---
+++ src/components/header/GlobalHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu mode="horizontal" :selected-keys="selectedKeys" @menu-item-click="doMenuClick">
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          <component :is="item.meta?.icon"> </component>
+          <span>{{ item.name }}</span>
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { routes } from '@/router/routes'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import checkAccess from '@/access/checkAccess'
+
+const store = useStore()
+const router = useRouter()
+
+const user = store.state.user
+const visibleRoutes = computed(() => {
+  const loginUser = user.loginUser
+  return routes.filter((item) => {
+    if (item.meta?.hideInMenu || item.path === '/user') {
+      return false
+    }
+
+    console.log(loginUser, item.meta?.access)
+    return checkAccess(loginUser, item.meta?.access)
+  })
+})
+// 默认主页
+const selectedKeys = ref(['/'])
+
+// 路由跳转后，更新选中的菜单项的高亮
+router.afterEach((to) => {
+  selectedKeys.value = [to.path]
+})
+const doMenuClick = (key) => {
+  if (key === '0') {
+    router.push({
+      path: '/'
+    })
+  } else {
+    router.push({
+      path: key
+    })
+  }
+}
+</script>
+
+<style scoped>
+#globalHeader {
+  height: 70px;
+  border-bottom: 1px solid #eee;
+}
+.title-bar {
+  padding-top: 10px;
+}
+
+:deep(.arco-menu-horizontal .arco-menu-inner) {
+  height: 70px;
+  padding: 0px 20px;
+}
+</style>
